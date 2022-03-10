d904b2161dd905aac0a58368a505b56ce2c77e6d
Haotian Mo <toreimimo@gmail.com>
2022-03-09 22:45:53 -0500

    Initial Commit

---
+++ bilibiliLivestreamTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bilibiliLivestreamTracker
+//
+//  Created by junius7even on 2022-03-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bilibiliLivestreamTracker/bilibiliLivestreamTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  bilibiliLivestreamTrackerApp.swift
+//  bilibiliLivestreamTracker
+//
+//  Created by junius7even on 2022-03-09.
+//
+
+import SwiftUI
+
+@main
+struct bilibiliLivestreamTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
