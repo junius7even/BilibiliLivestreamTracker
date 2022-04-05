42bb10425e2f413cf560fa23cd8904e70b4f168f
Haotian Mo <toreimimo@gmail.com>
2022-04-04 21:07:06 -0400

    Add toolbar item to add streamers
    - Add plus.diamond tool bar item
    - Modify tool bar design to conform to both dark and light mode

--- bilibiliLivestreamTracker/Views/MainPageView.swift
+++ bilibiliLivestreamTracker/Views/MainPageView.swift
@@ -11,6 +11,7 @@ struct MainPageView: View {
     @StateObject var model = ContentModel()
     @State var liveStatus = -1
     @State var hasAppearedOnce = false
+    @State var showSheet = false
     var body: some View {
         TabView {
             // Bilibili tracker tab
@@ -28,24 +29,43 @@ struct MainPageView: View {
                         }
                     }
                 }
-                .navigationTitle("哔哩哔哩Tracker v1.0")
+                .navigationBarTitle("哔哩哔哩Tracker v1.0", displayMode: .inline)
                 .toolbar {
-                    // Refresh button
-                    Button {
-                        model.isFetching = true
-                        // Removes everything from the array to clear the original screen
-                        model.allStreamerInfo.removeAll()
-                        model.getAllLiveRoomStatus(IdArray: BluesisConstants.BluesisIDCollection)
-                    } label: {
-                        HStack {
-                            Text("Refresh")
-                            Image(systemName: "arrow.clockwise")
+                    HStack {
+                        // Refresh button
+                        Button {
+                            model.isFetching = true
+                            // Removes everything from the array to clear the original screen
+                            model.allStreamerInfo.removeAll()
+                            model.getAllLiveRoomStatus(IdArray: BluesisConstants.BluesisIDCollection)
+                        } label: {
+                            HStack {
+                                Text("Refresh")
+                                Image(systemName: "arrow.clockwise")
+                            }
+                            .foregroundColor(Color(UIColor.systemGray6))
+                            .padding(4)
+                            .background(Color.primary)
+                            .cornerRadius(15)
+                        }
+                        // Add more livers button
+                        Button {
+                            showSheet.toggle()
+                        } label: {
+                            Image(systemName: "plus.diamond")
+                                .foregroundColor(Color.primary)
                         }
                     }
                 }
             }
+            .sheet(isPresented: $showSheet, onDismiss: {
+                
+            }, content: {
+                Text("Hi")
+            })
             .tabItem({
                 Image("biliTele2")
+                    .foregroundColor(Color(uiColor: .systemGray6))
                 Text("Bilibili")
             })
             .onAppear {
